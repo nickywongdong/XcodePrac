c95828981a4b91b7fc4a11b4c1b3be66a03080de
wongnich <wongnich@gmail.com>
2017-02-15 16:40:13 -0800

    todolist

---
+++ Practice/CreateTaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateTaskViewController.swift
+//  Practice
+//
+//  Created by Nicholas Wong on 2/15/17.
+//  Copyright © 2017 Nicholas Wong. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = ViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: Any) {
+        //create a task with a task from the outlet information
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        //Add new task to array in previous viewcontroller
+        
+    }
+}
---
+++ Practice/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  Practice
+//
+//  Created by Nicholas Wong on 2/15/17.
+//  Copyright © 2017 Nicholas Wong. All rights reserved.
+//
+
+import Foundation
+
+
+class Task{
+    var name = ""
+    var important = false
+}
